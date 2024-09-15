0c58ce10680f952dd9c2c0399a8ccf117b3636d9
RidwanOladipo <oladiporidwan10@gmail.com>
2024-09-15 15:09:36 +0100

    Enhanced password saving functionality

--- main.py
+++ main.py
@@ -43,8 +43,16 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any field empty.")
     else:
+        with open("data.json", "r") as data_file:
+            # Reading old data
+            data = json.load(data_file)
+            # Updating old data with new data
+            data.update(new_data)
+
         with open("data.json", "w") as data_file:
-            json.dump(new_data, data_file, indent=4)
+            # Saving updated data
+            json.dump(data, data_file, indent=4)
+
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
